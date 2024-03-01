94f2ff31e66a896c695a7bc2f261299aac9d49bd
Facundo <facundo962010@hotmail.com>
2024-03-01 19:33:30 -0300

    first commit

---
+++ src/data/data.ts
@@ -0,0 +1,117 @@
+export const SkillsData = [
+    {
+        list: [
+
+            {
+                name: "HTML5",
+                icon: "/images/html.svg"
+            },
+
+            {
+                name: "CSS3",
+                icon: "/images/css.svg"
+            },
+
+            {
+                name: "JavaScript",
+                icon: "/images/js.svg"
+            },
+
+            {
+                name: "ReactJS",
+                icon: "/images/react.svg"
+            },
+
+            {
+                name: "Astro",
+                icon: "/images/astro.svg"
+            },
+
+            {
+                name: "NextJs",
+                icon: "/images/next.svg"
+            },
+
+            {
+                name: "Typescript",
+                icon: "/images/ts.svg"
+            },
+
+            {
+                name: "SaSS",
+                icon: "/images/sass.svg"
+            },
+
+            {
+                name: "TailWindcss",
+                icon: "/images/tailwind.svg"
+            },
+
+            {
+                name: "MaterialUI",
+                icon: "/images/material.svg"
+            },
+            {
+                name: "NodeJS",
+                icon: "/images/node-js.svg"
+            },
+
+            {
+                name: "Docker",
+                icon: "/images/docker.svg"
+            },
+
+        ]
+    }
+];
+
+
+export const ProjectsData = [
+    {
+        projects: [
+
+            {
+                name: "Bienes Raíces Blog",
+                description: "Página estática sobre ventas de casas, incluye su modo oscuro hecho con JavaScript, además también posee un blog con sus respectivas entradas. Esta página fue uno de mis primeros proyectos, está hecho con HTML, CSS3 y JavaScript.",
+                image: "/images/bienes-raices.png",
+                link: "https://testingjs.netlify.app/",
+                github: 'https://github.com/facundo-cejas9/bienes.raices',
+            },
+
+            {
+                name: "Encriptador",
+                description: "Proyecto realizado en el curso de Allura, brindado por Oracle. Consiste en un encriptador de texto: un input donde ingresamos un texto y lo encripta, sustituyendo las vocales por otras letras. Funciona tanto para encriptar como para desencriptar, además cuenta con un botón de copiar. Se utilizó JavaScript y HTML.",
+                image: "/images/encriptador.png",
+                link: "https://fanciful-kashata-73eef3.netlify.app/",
+                github: 'https://github.com/facundo-cejas9/encryptallura',
+            },
+
+            {
+                name: "Cotizador",
+                description: "Proyecto realizado para prácticas del curso de React de Juan de la Torre. Consiste en un cotizador que te proporciona el precio actualizado del dólar a pesos argentinos, así como también el de criptomonedas a pesos argentinos. Se implementó ReactJS y CSS puro para su diseño.",
+                image: "/images/cotizador.png",
+                link: "https://kaleidoscopic-salamander-be5b2c.netlify.app/",
+                github: 'https://github.com/facundo-cejas9/cotizador',
+
+            },
+
+            {
+                name: "AlkyBank",
+                description: "Proyecto realizado en la aceleración de Alkemy de ReactJS, la cual consistió en armar un dashboard para una simulación de un banco. Este incluía una página de inicio de sesión, una página de registro y la capacidad de realizar transacciones como enviar y recibir dinero. Se implementaron React y CSS Modules.",
+                image: "/images/alkybank.png",
+                link: "https://github.com/facundo-cejas9/alkybank",
+                github: "https://github.com/facundo-cejas9/alkybank",
+            },
+
+            {
+                name: "Jounrnal App",
+                description: "Aplicación creada en React con el curso de Fernando Herrera. La aplicación cuenta con funcionalidades de registro y inicio de sesión utilizando Firebase como base de datos. Permite crear notas y recordatorios estilo agenda, además de la capacidad de subir imágenes. Se utilizó React y MaterialUI para los estilos.",
+                image: "/images/journal.png",
+                link: "https://journalv2.netlify.app/",
+                github: "#",
+            }
+
+           
+        ]
+    }
+]
